a34cde899970c77aae3c8cf28ac6cb5980c45aa4
afadfath03 <afadfath03@gmail.com>
2024-05-30 13:35:08 +0700

    Update Modular

--- Meet 6 - Modular/page/page.php
+++ Meet 6 - Modular/page/page.php
@@ -2,17 +2,9 @@
 if($_GET['mod'] == 'home'){
     include 'home/home.php';
 } else if ($_GET['mod'] == 'berita') {
-    include 'berita/berita.php';
+    include 'berita/berita.php';    
 } else if($_GET['mod'] == 'berita_detail'){
     include 'berita/berita_detail.php';
 }
-// } else if ($_GET['mod'] == 'lifestyle') {
-    
-// } else if ($_GET['mod'] == 'education') {
-    
-// } else if ($_GET['mod'] == 'technology') {
-    
-// } else if ($_GET['mod'] == 'sport') {
-    
-// }
+
 ?>
